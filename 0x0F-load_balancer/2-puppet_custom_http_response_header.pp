7471cd65f569f49d260e9e8f7116b05cff94031e
thatguykwameC <thatguykwamec@gmail.com>
2024-04-30 00:21:48 -0500

    Update a custom HTTP header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+#Add a custom HTTP header with Puppet
+
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/bin', '/bin'],
+}
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Configure Nginx to add the custom HTTP header
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+
+# Ensure Nginx is running
+service { 'nginx':
+  ensure => running,
+}
